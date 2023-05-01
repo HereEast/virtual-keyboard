884670236521f8b7777e88add0d4415017d21270
Here East <nastiapiven@gmail.com>
2023-05-01 19:08:20 +0200

    feat: update dom structure

--- src/js/utils.js
+++ src/js/utils.js
@@ -17,16 +17,24 @@ function createElement(tagName, ...classNames) {
 
 // DOM
 function createDOMStructure() {
+    const text = `This keyboard was created for Mac OS. <br> To delete prev character use <span>DELETE</span>, to delete next character use <span>CTRL + D</span>`;
+
     const page = createElement("div", "page");
     const main = createElement("main", "main");
+    const introContainer = createElement("div", "intro__container");
     const textareaContainer = createElement("div", "textarea__container");
     const textarea = createElement("textarea", "textarea");
     const keyboard = createElement("div", "keyboard");
     const keyboardContainer = createElement("div", "keyboard__container");
 
+    const description = createElement("p", "description");
+    description.innerHTML = text;
+
+    introContainer.append(description);
+
     textareaContainer.append(textarea);
     keyboard.append(keyboardContainer);
-    main.append(textareaContainer, keyboard);
+    main.append(introContainer, textareaContainer, keyboard);
     page.append(main);
     body.prepend(page);
 }
